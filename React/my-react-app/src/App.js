1678341234a321ade2d0b0135e913868fdac04f6
JLouisse088 <louissepolong890@gmail.com>
2024-09-25 15:57:07 +0800

    react reset-up

--- React/my-react-app/src/App.js
+++ React/my-react-app/src/App.js
@@ -1,20 +1,31 @@
-import Home from './components/Home';
-
-import { Router, Routes, Route } from 'react-router-dom';
-
-
-
+import logo from './logo.svg';
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-        </Routes>
-      </Router>
+      <Routes>
+        <Route path='/home' element={<h1>Hello</h1>} />
+      </Routes>
     </>
 
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
   );
 }
 

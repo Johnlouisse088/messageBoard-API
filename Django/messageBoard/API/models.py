a34080b53b92bcb50f4452213e0493c228e49eb2
JLouisse088 <louissepolong890@gmail.com>
2024-09-26 21:09:06 +0800

    add custom user

--- Django/messageBoard/API/models.py
+++ Django/messageBoard/API/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+
+class User(AbstractUser):
+    name = models.CharField(max_length=200, null=True)
+    email = models.EmailField(unique=True, null=False,)
+    bio = models.TextField(null=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+    def __str__(self):
+        return self.email
+

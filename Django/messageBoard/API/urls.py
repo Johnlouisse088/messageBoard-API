cab9f622e7b7b30fb08ffd94cda8b21a2fdb549a
JLouisse088 <louissepolong890@gmail.com>
2024-10-05 17:32:36 +0800

    added serializers and objects

--- Django/messageBoard/API/urls.py
+++ Django/messageBoard/API/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("routes/", views.routes, name="routes")
+    path("routes/", views.routes, name="routes"),
+    path("", views.home, name="home")
 ]
 
--- Django/messageBoard/API/views.py
+++ Django/messageBoard/API/views.py
@@ -1,5 +1,8 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
+from .models import Room, Topic, Message
+from .serializers import RoomSerializer, TopicSerializer, MessageSerializer
+
 # Create your views here.
 
 @api_view(['GET'])
@@ -82,3 +85,18 @@ def routes(request):
     return Response(routes)
 
 
+@api_view(['GET'])
+def home(request):
+    rooms = Room.objects.all()
+    topics = Topic.objects.all()
+    messages = Message.objects.all()
+
+    room_serializer = RoomSerializer(rooms, many=True)
+    topic_serializer = TopicSerializer(topics, many=True)
+    message_serializer = MessageSerializer(messages, many=True)
+
+    return Response({
+        'rooms': room_serializer.data,
+        'topics': topic_serializer.data,
+        'messages': message_serializer.data
+    })
